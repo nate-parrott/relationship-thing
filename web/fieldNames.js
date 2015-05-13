54a5270593b52db0d343d1d8ac6a31397c5b9df1
Tauseef Khan <Tauseef@Tauseefs-MacBook-Pro.local>
2015-05-12 21:24:06 -0400

    added new visualizations and stats

--- web/fieldNames.js
+++ web/fieldNames.js
@@ -3,5 +3,6 @@ FIELD_NAMES = {
 	'relationship_quality': 'Self-assessed relationship quality (0 – 4)',
 	'respondent_yrsed': "Respondent's years of education",
 	'partner_yrsed': "Partner's years of education",
-	'how_long_ago_first_romantic': 'Relationship duration (years)'
+	'how_long_ago_first_romantic': 'Relationship duration (years)',
+	'hhinc': 'Household Income (dollars)'
 }

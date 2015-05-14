ebe4b210f36508ddf4cca6f1d6b67b0703219baa
nate-parrott <nateparro2t@gmail.com>
2015-05-14 02:45:22 -0400

    distribution tweaks

--- static/distributionPlot.js
+++ static/distributionPlot.js
@@ -60,7 +60,7 @@ function createDistributionPlot(div, params) {
 	// create finite # of buckets to hold the values:
 	var extents = d3.extent(values);
 	var nBuckets = params.buckets || 10;
-	var bucketSize = (extents[1] - extents[0]) / (nBuckets - 1);
+	var bucketSize = (extents[1] - extents[0]) / (nBuckets);
 	// put the values in the buckets:
 	var buckets = getBuckets(extents, nBuckets);
 	buckets.sort(function(a,b) {
@@ -102,7 +102,7 @@ function createDistributionPlot(div, params) {
 	// draw labels for the bars:
 	svg.selectAll('.bar-label').data(buckets).enter().append('text')
 	.attr('class', 'axis')
-	.text(function(bucket){ return bucket.toFixed(2) })
+	.text(function(bucket){ return bucket.toFixed(1) + '–' + (bucket+bucketSize).toFixed(1) })
 	.attr('x', function(bucket) {
 		return xScale(bucket) + (width / nBuckets - 2) / 2;
 	}).attr('text-anchor', 'start').attr('y', function() {return height + 20});
@@ -148,15 +148,28 @@ function createDistributionPlot(div, params) {
 		var bucketPercentages = segments[i];
 		var color = colors[i % colors.length];
 		
+		var START = 'START';
+		var END = 'END';
+		
 		var path = d3.svg.area().x(function(bucket) {
-			return xScale(bucket + bucketSize / 2);
+			if (bucket === START) {
+				return xScale(extents[0]);
+			} else if (bucket === END) {
+				return xScale(extents[1]);
+			} else {
+				return xScale(bucket + bucketSize / 2);
+			}
 		}).y0(function(bucket) {
 			return height;
 		}).y1(function(bucket) {
-			return yScale(bucketPercentages[bucket]);
+			if (bucket === START || bucket === END) {
+				return yScale(0);
+			} else {
+				return yScale(bucketPercentages[bucket]);
+			}
 		}).interpolate('cardinal');
-		
-		svg.append('path').datum(buckets).attr({'class': 'line', 'd': path, 'opacity': 0.5, 'fill': color});
+		var data = [START].concat(buckets, [END]);
+		svg.append('path').datum(data).attr({'class': 'line', 'd': path, 'opacity': 0.5, 'fill': color});
 	}
 	
 	if (params.segment) {

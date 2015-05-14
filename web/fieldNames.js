3bc8c2eb4be243fbc194955657d37187782c1a47
nate-parrott <nateparro2t@gmail.com>
2015-05-13 21:15:44 -0400

    remove ~ files

--- web/fieldNames.js
+++
@@ -1,9 +0,0 @@
-FIELD_NAMES = {
-	'age_difference': 'Age difference',
-	'relationship_quality': 'Self-assessed relationship quality (0 – 4)',
-	'respondent_yrsed': "Respondent's years of education",
-	'partner_yrsed': "Partner's years of education",
-	'how_long_ago_first_romantic': 'Relationship duration (years)',
-	'hhinc': 'Household Income (dollars)',
-	'parental_approval': 'Parental Approval'
-}

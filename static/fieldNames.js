92ce3dd5c8677bcf1db7e05090ac6165ed4f3277
jsl15 <jessicasliang@gmail.com>
2015-05-15 00:14:41 -0700

    added some attractiveness graphs to the webpage

--- static/fieldNames.js
+++ static/fieldNames.js
@@ -5,8 +5,9 @@ FIELD_NAMES = {
 	'partner_yrsed': "Partner's years of education",
 	'how_long_ago_first_romantic': 'Relationship duration (years)',
 	'hhinc': 'Household Income (dollars)',
-	'parental_approval': 'Parental approval'
-	'q32_internet': 'Met through Internet?'
+	'parental_approval': 'Parental approval',
+	'q32_internet': 'Met through Internet?',
+	'w4_attractive_partner': 'Partner Attractiveness (0-4)'
 }
 
 FIELD_VALUE_NAMES = {
@@ -17,7 +18,7 @@ FIELD_VALUE_NAMES = {
 	'parental_approval': {
 		0: 'Neither parent approves',
 		1: 'At least one parent approves'
-	}
+	},
 	'q32_internet': {
 		0: 'Met on the Internet',
 		1: 'Did not meet on Internet'
